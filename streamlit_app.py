093c836285123376cd89e14a43e9c11433408b32
mirellafmarques <mirellamarques@id.uff.br>
2025-05-04 20:09:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,12 @@ import streamlit as st
 from datetime import datetime
 import numpy as np
 from PIL import Image
-#magnetic
 from datetime import datetime
 
 st.set_page_config(layout="wide", page_title="Geographic")  # Esta linha deve vir antes de qualquer st.write(), st.title(), etc.
 
 # Construindo o Sidebar
-st.sidebar.title("Geographic")
+st.sidebar.title("Aplicações Geograficas")
 #page = st.sidebar.radio("Aplicações", ["Geografico", "Magnetico"])
 st.sidebar.image("Earth.jpeg")
 st.sidebar.title("About")
