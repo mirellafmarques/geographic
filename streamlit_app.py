17d24c18bda570b66493345558c3923800e974a3
mirellafmarques <mirellamarques@id.uff.br>
2025-05-05 12:38:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,7 @@ st.sidebar.title("Aplicações Geoespaciais")
 #page = st.sidebar.radio("Aplicações", ["Geografico", "Magnetico"])
 st.sidebar.image("Terra.png")
 st.sidebar.title("Sobre")
-st.sidebar.info("Este aplicativo foi desenvolvido para a demonstração de aplicação de bibliotecas para a disciplina de Desenvolvimento de Aplicações Geoespaciais.")
-
+st.sidebar.info("Este aplicativo serve como demonstração prática da aplicação de bibliotecas relevantes para a disciplina de Desenvolvimento de Aplicações Geoespaciais.")
 #--------------------------------------------------------------------------------------
 st.title("Aplicações Geoespaciais")
 st.subheader("Ferramentas para cálculos geodésicos e modelos geomagnéticos")
